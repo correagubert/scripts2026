0ad781c98ec92f5675f541baf8b0c6dc99762ed2
correagubert <correagubert@mpscans.com.br>
2026-09-19 15:36:23 -0300

    Cool stuff. Cool stuff.

--- aulapython-nicolas/semana02/atividade-calcSalAliq.py
+++ aulapython-nicolas/semana02/atividade-calcSalAliq.py
@@ -1,8 +1,10 @@
+import sys
 salBruto = float(input("Insira o seu salário, em reais e centavos: "))
 if salBruto < 1621.01: aliqValor = 0.075
 elif salBruto < 2902.85: aliqValor = 0.09
 elif salBruto < 4354.28: aliqValor = 0.12
-else: aliqValor = 0.14
+elif salBruto < 8475.55: aliqValor = 0.14
+else: print("Alíquota indefinida."), sys.exit(0)
 roubo = salBruto*aliqValor
 salFinal = salBruto - roubo
 print(f"Alíquota de {aliqValor*100:.2f}%, imposto de R${roubo:.2f} - Salário final: {salFinal:.2f}")
